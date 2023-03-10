b86cbb9a706a1ffde260931f5d2877ad5a761435
dailar <dailarse00@gmail.com>
2023-03-10 16:52:31 +0300

    t2

--- task2-observer-pattern/src/main/java/com/mjc/stage2/impl/RectangleObserver.java
+++ task2-observer-pattern/src/main/java/com/mjc/stage2/impl/RectangleObserver.java
@@ -1,5 +1,22 @@
 package com.mjc.stage2.impl;
 
-public class RectangleObserver  {
-    // Write your code here!
+import com.mjc.stage2.Observer;
+import com.mjc.stage2.entity.Rectangle;
+import com.mjc.stage2.entity.RectangleValues;
+import com.mjc.stage2.event.RectangleEvent;
+import com.mjc.stage2.warehouse.RectangleWarehouse;
+
+public class RectangleObserver implements Observer {
+    private final RectangleWarehouse rectangleWarehouse = RectangleWarehouse.getInstance();
+
+    @Override
+    public void handleEvent(RectangleEvent event) {
+        Rectangle source = event.getSource();
+        double sideA = source.getSideA();
+        double sideB = source.getSideB();
+        double square = sideA * sideB;
+        double perimeter = 2 * (sideA + sideB);
+        RectangleValues rectangleValues = new RectangleValues(square, perimeter);
+        rectangleWarehouse.put(source.getId(), rectangleValues);
+    }
 }
