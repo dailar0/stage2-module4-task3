12b1a5c9e2111c6f5bf5fe6a289edcd57012be62
dailar <dailarse00@gmail.com>
2023-03-11 18:32:11 +0300

    t3 2/2?

--- task3-handling/src/main/java/com/mjc/stage2/parser/LexemeParser.java
+++ task3-handling/src/main/java/com/mjc/stage2/parser/LexemeParser.java
@@ -1,14 +1,19 @@
 package com.mjc.stage2.parser;
 
 import com.mjc.stage2.entity.AbstractTextComponent;
+import com.mjc.stage2.entity.SymbolLeaf;
+import com.mjc.stage2.entity.TextComponentType;
 
-public class LexemeParser extends AbstractTextParser{
+import java.util.Arrays;
+
+public class LexemeParser extends AbstractTextParser {
     private static final String LEXEME_REGEX = "\\s+";
     private static final String WORD_REGEX = "\\w[\\w!=?():]+";
 
     @Override
     public void parse(AbstractTextComponent abstractTextComponent, String string) {
-
+        string.chars()
+                .forEach(value ->
+                        abstractTextComponent.add(new SymbolLeaf(TextComponentType.WORD, (char) value)));
     }
-
 }
--- task3-handling/src/main/java/com/mjc/stage2/parser/WordParser.java
+++ task3-handling/src/main/java/com/mjc/stage2/parser/WordParser.java
@@ -2,10 +2,16 @@ package com.mjc.stage2.parser;
 
 
 import com.mjc.stage2.entity.AbstractTextComponent;
+import com.mjc.stage2.entity.SymbolLeaf;
+import com.mjc.stage2.entity.TextComponentType;
 
 public class WordParser extends AbstractTextParser{
     @Override
     public void parse(AbstractTextComponent abstractTextComponent, String string) {
+        char[] arr = string.toCharArray();
 
+        for (char x : arr) {
+            abstractTextComponent.add(new SymbolLeaf(TextComponentType.WORD, x));
+        }
     }
 }
